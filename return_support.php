1cf6a75b336fd166cacff7b7eec9211b45fa98ef
JG <jgalbraith@boostability.com>
2014-04-04 13:56:00 -0600

    REALLY FIXED SUPPORT BUTTON AGAIN

--- return_support.php
+++ return_support.php
@@ -3,6 +3,6 @@ include 'users/loginheader.php';
 
 $domain = $_POST['domain'];
 
-$query = "UPDATE DomainDetails SETDevStart='0000-00-00' WHERE Domain ='$domain'";
+$query = "UPDATE DomainDetails SET DevStart='0000-00-00' WHERE Domain ='$domain'";
 mysqli_query($mysqli, $query);
 ?>
